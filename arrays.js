8458d5fb8e5bca1fa98d3f618f12d66b98937a75
Leandro Reis <leandro.reiz@gmail.com>
2020-01-05 22:57:49 -0300

    added more arrays exercises

--- arrays.js
+++ arrays.js
@@ -1,3 +1,4 @@
+/*
 const items = [
     { name: 'Bike',     price: 100  },
     { name: 'TV',       price: 200  },
@@ -72,3 +73,131 @@ const includesTen = valueItems.includes(10);
 
 console.log(includesTwo);
 console.log(includesTen);
+
+//-------------------------------------------
+// 1. Create an Array
+var a = ['first', 'second'];
+
+console.log('a\'s contents:', a);
+console.log('a\'s length:', a.length);
+
+// 2. Access (Index Into) an Array Item
+let a = ['first', 'second'];
+
+// first = 'first'
+let first = a[0]; 
+
+// last = 'second'
+let last = a[a.length - 1]; 
+
+console.log('a[0]:', first);
+console.log('a[a.length - 1]:', last);
+
+// 3. Loop over an array
+var a = ['first', 'second'];
+
+a.forEach(function(e, i, array) {
+    // 'i' is the index
+    // 'e' is the element
+    console.log(i + ' ' + e);
+});
+
+// 4. Append to the end of an array
+var a = ['first', 'second'];
+
+// Append 'third' to array 'a'
+a.push('third');
+
+console.log('a:', a);
+
+// 5. Remove from the end of an array
+var a = ['first', 'second', 'third'];
+console.log('Original Array:', a);
+
+// Remove the last element from the array
+let removed = a.pop();
+
+console.log('Modified Array:', a);
+console.log('Removed Element:', removed);
+
+// 6. Remove from the front of an array
+var a = ['first', 'second', 'third'];
+console.log('Original Array:', a);
+
+// Remove the first element from the array
+let removed = a.shift();
+
+console.log('Modified Array:', a);
+console.log('Removed Element:', removed);
+
+// 7. Add to the front of an array
+var a = ['first', 'second', 'third'];
+console.log('Original Array:', a);
+
+// Insert element at the beginning of the array
+a.unshift('fourth');
+
+console.log('Modified Array:', a);
+
+// 8. Find the index of an item in the array
+var a = ['first', 'second', 'third', 'fourth'];
+
+let position = a.indexOf('second');
+
+console.log('a:', a);
+console.log('position:', position);
+
+// 9. Remove an item by index position
+var a = ['first', 'second', 'third', 'fourth', 'fifth'];
+console.log('Original Array: ', a);
+
+let position = 1;
+let elementsToRemove = 2;
+// Remove 'elementsToRemove' element(s) starring at 'position'
+a.splice(position, elementsToRemove);
+
+console.log('Modified Array: ', a);
+
+// 10. Copy an array
+var a = ['first', 'second', 'third', 'fourth'];
+console.log('a: ', a);
+
+// Shallow copy array 'a' into a new object
+let b = a.slice();
+
+console.log('b: ', b);
+
+// 11. Sort an array
+var a = ['c', 'a', 'd', 'b', 'aa'];
+var b = [9, 2, 13, 7, 1, 12, 123];
+
+// Sort in ascending lexicographical order using a built-in
+a.sort();
+b.sort();
+
+console.log('a: ', a);
+console.log('b: ', b);
+
+// Sort in descending lexicographical order using a compare function
+var a = ['c', 'a', 'd', 'b', 'aa'];
+var b = [9, 2, 13, 7, 1, 12, 123];
+
+a.sort(function(x, y) { return x < y });
+b.sort(function(x, y) { return x < y });
+
+console.log('a: ', a);
+console.log('b: ', b);
+
+// Sort in descending lexicographical order using a compare arrow function
+var a = ['c', 'a', 'd', 'b', 'aa'];
+
+a.sort((x, y) => x < y);
+
+console.log('a: ', a);
+*/
+// 12. Iterate over an array
+var a = ['first', 'second', 'third', 'fourth'];
+
+for (let e of a) {
+    console.log('e: ', e);
+}
--- loops.js
+++ loops.js
@@ -1,4 +1,4 @@
-/*const colorsArray = ['red', 'blue', 'green', 'yellow'];
+const colorsArray = ['red', 'blue', 'green', 'yellow'];
 
 //using for loops
 for (let i = 0; i < colorsArray.length; i++) {
@@ -15,23 +15,4 @@ numbers.forEach(number => result.push(number * 2));
 console.log(result);
 
 //using maps
-console.log(resultMap = numbers.map(number => number * 2));*/
-
-var i = 12;
-var d = 4.0;
-var s = 'Foda-se, ';
-
-// Declare second integer, double, and String variables.
-let secondInteger;
-let SecondDouble;
-let secondString;
-// Read and save an integer, double, and String to your variables.
-
-
-// Print the sum of both integer variables on a new line.
-console.log(integer);
-// Print the sum of the double variables on a new line.
-console.log(double);
-// Concatenate and print the String variables on a new line
-// The 's' variable above should be printed first.
-console.log(string);
+console.log(resultMap = numbers.map(number => number * 2));
