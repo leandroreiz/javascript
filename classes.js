d42dae98e41495dd1991ba524c345b251ad4a52c
Leandro Reis <leandro.reiz@gmail.com>
2020-01-10 23:44:52 -0300

    added template literals exercises

--- classes.js
+++ classes.js
@@ -349,6 +349,33 @@ catch (exception) {
     console.log(exception.name + ': ' + exception.message
     + ' (collar is a method of Dog, not Animal).');
 }
-
-
 */
+class Rectangle {
+    constructor(w, h) {
+        this.w = w;
+        this.h = h;
+    }
+}
+
+/*
+ *  Write code that adds an 'area' method to the Rectangle class' prototype
+ */
+Rectangle.prototype.area = function() {
+    return this.w * this.h;
+}
+
+/*
+ * Create a Square class that inherits from Rectangle and implement its class constructor
+ */
+class Square extends Rectangle {
+    constructor(w, h) {
+        super(w);
+        this.h = w; // squares have the 4 sides with the same size
+    }
+}
+
+const rec = new Rectangle(3, 4);
+const sqr = new Square(3);
+
+console.log(rec.area());
+console.log(sqr.area());
