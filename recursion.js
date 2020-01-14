7869c17974847cbec26d968438dfac643ed9a1d4
Leandro Reis <leandro.reiz@gmail.com>
2020-01-14 01:31:36 -0300

    added more recursive exercises

--- recursion.js
+++ recursion.js
@@ -1,3 +1,4 @@
+/*
 let factorial = function fac(num) {
     if (num === 1) {
         // base case
@@ -22,4 +23,37 @@ const final = factorial(5);
  *      5 * 24 = 120 (result)
  * 
  * Using the Google Dev tools (debugger) you can easily see all the process. When the functions are stacking and, after reaching the end, unstacking.
- */
+ 
+
+
+function countDown(num) {
+    // base case
+    if (num <= 0) {
+        console.log('All done!');
+        return;
+    }
+    console.log(num);
+
+    // recursive case
+    num--;
+    countDown(num);
+}
+
+countDown(4);
+*/
+
+// The Collatz Conjecture Exercise
+
+function collatz(n) {
+    count = 0;
+
+    if (n === 1) {
+        return 0;
+    } else if (n % 2 === 0) {
+        return count = 1 + collatz(n/2);
+    } else {
+        return count = 1 + collatz(3*n + 1);
+    }
+}
+
+console.log(collatz(3));
